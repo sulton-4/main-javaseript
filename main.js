67af390f13e5377bcff35c310f2e9b83c15774a5
xasanof17 <xasanof17@gmail.com>
2023-06-03 18:39:29 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var a = +prompt('birinchi sonni kiriting')
+var b = +prompt('ikkinchi sonni kiriting')
+var c = +prompt('uchinchi sonni kiriting')
+
+if (a > b < c && a > b < c) {
+    console.log(b);
+}   
+else if (b > a < c && b > a < c) {
+    alert(a);
+}
+else if (b > c < a && b > c < a) {
+    alert(c);
+}
+else("Xatolik")
